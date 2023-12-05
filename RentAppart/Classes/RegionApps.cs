83989f99703b4fcb3af7e152843aa84379b95e10
Таланов Денис <denist.talanov.gmail.com@mail.ru>
2023-12-05 23:24:34 +0300

    added a partial class

---
+++ RentAppart/Classes/RegionApps.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentApparts.Classes
+{
+    public partial class RegionApps : Appartment
+    {
+        protected string Region { get; set; }
+
+    }
+}
---
+++ RentAppart/Classes/RegionApps2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace RentApparts.Classes
+{
+
+    public partial class RegionApps : Appartment
+    {
+        // Константа
+        public const int MaxApartments = 100;
+
+        // Read-only поле
+        public readonly DateTime CreationDate;
+
+        // Статическое поле
+        private static int apartmentCount;
+
+        // Статический метод
+        public static int GetApartmentCount(){
+            return apartmentCount;
+        }
+
+        // Конструктор
+        public RegionApps()
+        {
+            CreationDate = DateTime.Now;
+            apartmentCount++;
+        }
+
+        // Реализация виртуального метода базового класса
+        public override void DisplayInfo()
+        {
+            base.DisplayInfo();
+            Console.WriteLine($"Region: {Region}");
+        }
+    }
+}
